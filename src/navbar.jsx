b69a567a3ff26c450860ef387206c6d853cb311b
yahyaouinour <nouryahyaoui734@gmail.com>
2024-09-24 14:32:56 +0100

    first commit

---
+++ src/navbar.jsx
@@ -0,0 +1,9 @@
+import app from "./App";
+
+function Navbar(props){
+    return(
+     <div className="bg-green-800 text-center traking-widest text-white py-10 fond-bold  text-tx  w-screen uppercase ">hello </div>
+    );
+}
+
+export default app;
